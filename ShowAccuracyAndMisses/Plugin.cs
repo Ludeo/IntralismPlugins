ba4ab5c57a4e1526c3736b0aaddccde6d14735e3
Ludeo <lucas.steuer1@gmx.de>
2023-03-27 01:17:41 +0200

    Init

---
+++ ShowAccuracyAndMisses/Plugin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShowAccuracyAndMisses
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static ManualLogSource logger;
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        private static GameObject missTextObject;
+        private static Text missText;
+        private static GameObject accuracyTextObject;
+        private static Text accuracyText;
+
+        private void Awake()
+        {
+            logger = this.Logger;
+            this.harmony.PatchAll(typeof(Plugin));
+            logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameScene), "Start")]
+        public static void GameSceneStart(GameScene __instance)
+        {
+            logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} game scene started");
+            GameObject ingameUiCanvas = __instance.ingameUICanvas;
+
+            if (!missTextObject)
+            {
+                missTextObject = new GameObject
+                {
+                    transform = {
+                        parent = ingameUiCanvas.transform,
+                        position = new Vector3(975, 530, 0),
+                    },
+                    layer = ingameUiCanvas.layer,
+                };
+
+                missText = missTextObject.AddComponent<Text>();
+                missText.rectTransform.sizeDelta = new Vector2(150, 18);
+                missText.text = "Misses: " + (__instance.pbase.fullLevelData.mapData.maxLives - __instance.pbase.lives);
+                missText.fontSize = 16;
+                missText.font = Font.GetDefault();
+            }
+
+            missTextObject.SetActive(__instance.gameMode != GameScene.GameMode.Hardcore);
+
+            if (!accuracyTextObject)
+            {
+                accuracyTextObject = new GameObject
+                {
+                    transform = {
+                        parent = ingameUiCanvas.transform,
+                        position = new Vector3(975, 555, 0),
+                    },
+                    layer = ingameUiCanvas.layer,
+                };
+
+                accuracyText = accuracyTextObject.AddComponent<Text>();
+                accuracyText.rectTransform.sizeDelta = new Vector2(150, 18);
+                accuracyText.text = "Accuracy: " + (Math.Floor(__instance.pbase.accuracyScore * 10000.0) / 10000.0 * 100.0).ToString("0.00") + "%";
+                accuracyText.fontSize = 16;
+                accuracyText.font = Font.GetDefault();
+            }
+
+            accuracyTextObject.SetActive(true);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameScene), "Update")]
+        public static void GameSceneUpdate(GameScene __instance)
+        {
+            missText.text = "Misses: " + (__instance.pbase.fullLevelData.mapData.maxLives - __instance.pbase.lives);
+            accuracyText.text = "Accuracy: " + (Math.Floor(__instance.pbase.accuracyScore * 10000.0) / 10000.0 * 100.0).ToString("0.00") + "%";
+        }
+    }
+}
